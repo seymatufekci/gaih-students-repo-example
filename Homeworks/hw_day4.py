d2a84209bbc2d30ba78bba08c72c3931af255607
Seyma Tufekci <73655124+seymatufekci@users.noreply.github.com>
2021-02-04 19:44:16 +0300

    Add files via upload

---
+++ Homeworks/hw_day4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#Seyma Tufekci - @seymaatufekci
+#Homework-Day4
+
+print("Found prime numbers between 0 and 100:")
+print("")
+
+def find_prime():
+
+    for num in range(0, 100 + 1):
+        if (num == 1):
+            continue
+        if num > 1:
+            for i in range(2,num):
+                if(num % i == 0) :
+                    break
+            else:
+                print(num)
+
+find_prime()
+
